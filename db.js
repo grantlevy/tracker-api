9dcc24b406c193f3cca3d38f338b79999a19fc03
Grant <grantlevy7@gmail.com>
2020-07-24 21:13:48 -0400

    updates for database link

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb+srv://grantlevy:IssueTracker#1@issuetracker.siqif.mongodb.net/issuetracker?retryWrites=true';
+  const url = process.env.DB_URL || 'mongodb+srv://grantlevy:IssueTracker#1@issuetracker.siqif.mongodb.net/issuetracker?retryWrites=true&w=majority';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
